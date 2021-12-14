60189fa4f4faca08c5fe99706dbb37ec03cb5e2a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-14 11:32:21 +0100

    Establecidas las restricciones de la N, que es el tamaño del tablero

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,11 +3,16 @@
 print("¿Cuántos juegos quieres jugar?")
 T = int(input())
 if 1 <= T <= 10:
-        print("Número válido, seguimos")
+    print("Número válido, seguimos")
 else:
     print("Número NO válido,")
     print("¿Cuántos juegos quieres jugar?")
     T = int(input())
 print("De qué tamaño quieres el tablero")
 N=int(input())
-
+if 2 <= N <= 2000:
+    print("Número válido, seguimos")
+else:
+    print("Número NO válido,")
+    print("De qué tamaño quieres el tablero")
+    N = int(input())
