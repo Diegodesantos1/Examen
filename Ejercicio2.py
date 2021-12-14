20c31ce7615e0f1a2f6584d117d7a1de427ccff7
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-14 11:25:06 +0100

    Creadas las variables inputeables con el tamaño del tablero y el número de juegos

---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+#Ejercicio de las torres
+#T = Número de juegos a jugar, N= tamaño del tablero
+print("¿Cuántos juegos quieres jugar?")
+T = int(input()) #Debe ser un integer ya que son juegos enteros
+print("De qué tamaño quieres el tablero")
+N=int(input())
+for i in range(T):
