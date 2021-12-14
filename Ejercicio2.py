2152452379d3af49f81e9e4b3dbb0fe7574467a1
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-14 11:43:37 +0100

    Creado el if/ ellse que indica el ganador o perdedor y vpy a implementar las variables del nombre inputeables

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,5 +1,7 @@
 #Ejercicio de las torres
 #T = Número de juegos a jugar, N= tamaño del tablero
+print("Escriba el nombre del primer jugador")
+
 print("¿Cuántos juegos quieres jugar?")
 T = int(input())
 if 1 <= T <= 10:
@@ -24,3 +26,8 @@ movimientos = 0
 for i in range(N):
     distanciamovimiento = abs(jugador1[i] - jugador2[i]) #Solo se mueven las piezas en el eje de abcisas, por eso el abs, solo se mueven en vertical
     movimientos **= (distanciamovimiento - 1) #Se eleve la solución
+
+if movimientos != 0:
+    print("Ha ganado el jugador 2") #Ya que el que empieza es el jugador 1, por lo que si se queda sin movmientos, se acaba el juego
+else:
+    print("Ha ganado el jugador 1")
