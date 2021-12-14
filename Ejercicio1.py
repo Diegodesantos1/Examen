2d1e6954a7c6cb7c686277db321d3c3d22e56c99
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-14 10:45:49 +0100

    Creada la lista de vocales y la elección de nombres del jugador

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,11 @@
 #Ejercicio del minion
 
 def juego(string):
+    ListaVocales=['a','e','i','o','u','A','E','I','O','U'] #Tanto mayúsculas como minúsculas
+    print("Escriba el nombre del jugador 1")
+    nombrejugador1=str(input())
+    print("Escriba el nombre del jugador 2")
+    nombrejugador2=str(input())
     jugador1= 0
     jugador2 = 0
-    str_len = len(string)
-    for i in range(str_len):
+
